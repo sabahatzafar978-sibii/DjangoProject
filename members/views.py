3ebc85b5e134f6d6c4e701496c371ad1b3cfa32a
Sabahat <sabahatzafar978@gmail.com>
2026-07-04 22:15:50 +0500

    Initial Django project

---
+++ members/views.py
@@ -0,0 +1,40 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+from .models import Member
+from django.db.models import Q
+
+
+def main(request):
+    template = loader.get_template('main.html')
+    return HttpResponse(template.render({}, request))
+
+
+def members(request):
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def details(request, id):
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember': mymember,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def testing(request):
+    mydata = Member.objects.filter(
+        Q(firstname='Emil') | Q(firstname='Tobias')
+    ).values()
+
+    template = loader.get_template('template.html')
+    context = {
+        'mymembers': mydata,
+    }
+    return HttpResponse(template.render(context, request))
